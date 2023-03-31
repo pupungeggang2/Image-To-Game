752a013e1adf93be99baf22725607266c30ab7d7
pupungeggang2 <kyongkyong289@outlook.kr>
2023-03-31 19:24:48 +0900

    0.0.4 : Brush

--- Program/editor.py
+++ Program/editor.py
@@ -6,20 +6,74 @@ import pygame
 
 def draw_point(x, y):
     var.Image_Editor.brush_line_start = [x - UI.Game_Screen_Edit.rect[0], y - UI.Game_Screen_Edit.rect[1]]
-    pygame.draw.circle(var.Image_Editor.layer[var.Image_Editor.layer_selected], var.Image_Editor.brush_color, var.Image_Editor.brush_line_start, var.Image_Editor.brush_size / 2)
+
+    point_start_ul = [var.Image_Editor.brush_line_start[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.
+    brush_line_start[1] - var.Image_Editor.brush_size / 2]
+    point_start_ur = [var.Image_Editor.brush_line_start[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] - var.Image_Editor.brush_size / 2]
+    point_start_ll = [var.Image_Editor.brush_line_start[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] + var.Image_Editor.brush_size / 2]
+    point_start_lr = [var.Image_Editor.brush_line_start[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] + var.Image_Editor.brush_size / 2]
+
+    pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], var.Image_Editor.brush_color, [point_start_ll, point_start_lr, point_start_ur, point_start_ul])
 
 def draw_line(x, y):
     var.Image_Editor.brush_line_current = [x - UI.Game_Screen_Edit.rect[0], y - UI.Game_Screen_Edit.rect[1]]
-    pygame.draw.line(var.Image_Editor.layer[var.Image_Editor.layer_selected], var.Image_Editor.brush_color, var.Image_Editor.brush_line_start, var.Image_Editor.brush_line_current, var.Image_Editor.brush_size)
-    pygame.draw.circle(var.Image_Editor.layer[var.Image_Editor.layer_selected], var.Image_Editor.brush_color, var.Image_Editor.brush_line_start, var.Image_Editor.brush_size / 2)
+
+    point_cur_ul = [var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_size / 2]
+    point_cur_ur = [var.Image_Editor.brush_line_current[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_size / 2]
+    point_cur_ll = [var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_current[1] + var.Image_Editor.brush_size / 2]
+    point_cur_lr = [var.Image_Editor.brush_line_current[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_current[1] + var.Image_Editor.brush_size / 2]
+
+    point_start_ul = [var.Image_Editor.brush_line_start[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] - var.Image_Editor.brush_size / 2]
+    point_start_ur = [var.Image_Editor.brush_line_start[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] - var.Image_Editor.brush_size / 2]
+    point_start_ll = [var.Image_Editor.brush_line_start[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] + var.Image_Editor.brush_size / 2]
+    point_start_lr = [var.Image_Editor.brush_line_start[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] + var.Image_Editor.brush_size / 2]
+
+    if var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_line_start[0] > 0 and var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_line_start[1] > 0:
+        pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], var.Image_Editor.brush_color, [point_start_ll, point_start_ul, point_start_ur, point_cur_ur, point_cur_lr, point_cur_ll])
+
+    if var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_line_start[0] > 0 and var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_line_start[1] <= 0:
+        pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], var.Image_Editor.brush_color, [point_start_ul, point_start_ll, point_start_lr, point_cur_lr, point_cur_ur, point_cur_ul])
+
+    if var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_line_start[0] <= 0 and var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_line_start[1] > 0:
+        pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], var.Image_Editor.brush_color, [point_start_ul, point_start_ur, point_start_lr, point_cur_lr, point_cur_ll, point_cur_ul])
+
+    if var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_line_start[0] <= 0 and var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_line_start[1] <= 0:
+        pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], var.Image_Editor.brush_color, [point_start_ur, point_start_lr, point_start_ll, point_cur_ll, point_cur_ul, point_cur_ur])
+
     var.Image_Editor.brush_line_start = [x - UI.Game_Screen_Edit.rect[0], y - UI.Game_Screen_Edit.rect[1]]
 
 def erase_point(x, y):
     var.Image_Editor.brush_line_start = [x - UI.Game_Screen_Edit.rect[0], y - UI.Game_Screen_Edit.rect[1]]
-    pygame.draw.circle(var.Image_Editor.layer[var.Image_Editor.layer_selected], const.Color.erase, var.Image_Editor.brush_line_start, var.Image_Editor.brush_size / 2)
+
+    point_start_ul = [var.Image_Editor.brush_line_start[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.
+    brush_line_start[1] - var.Image_Editor.brush_size / 2]
+    point_start_ur = [var.Image_Editor.brush_line_start[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] - var.Image_Editor.brush_size / 2]
+    point_start_ll = [var.Image_Editor.brush_line_start[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] + var.Image_Editor.brush_size / 2]
+    point_start_lr = [var.Image_Editor.brush_line_start[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] + var.Image_Editor.brush_size / 2]
+
+    pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], const.Color.erase, [point_start_ll, point_start_lr, point_start_ur, point_start_ul])
 
 def erase_line(x, y):
     var.Image_Editor.brush_line_current = [x - UI.Game_Screen_Edit.rect[0], y - UI.Game_Screen_Edit.rect[1]]
-    pygame.draw.line(var.Image_Editor.layer[var.Image_Editor.layer_selected], const.Color.erase, var.Image_Editor.brush_line_start, var.Image_Editor.brush_line_current, var.Image_Editor.brush_size)
-    pygame.draw.circle(var.Image_Editor.layer[var.Image_Editor.layer_selected], const.Color.erase, var.Image_Editor.brush_line_start, var.Image_Editor.brush_size / 2)
-    var.Image_Editor.brush_line_start = [x - UI.Game_Screen_Edit.rect[0], y - UI.Game_Screen_Edit.rect[1]]
+
+    point_cur_ul = [var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_size / 2]
+    point_cur_ur = [var.Image_Editor.brush_line_current[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_size / 2]
+    point_cur_ll = [var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_current[1] + var.Image_Editor.brush_size / 2]
+    point_cur_lr = [var.Image_Editor.brush_line_current[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_current[1] + var.Image_Editor.brush_size / 2]
+
+    point_start_ul = [var.Image_Editor.brush_line_start[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] - var.Image_Editor.brush_size / 2]
+    point_start_ur = [var.Image_Editor.brush_line_start[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] - var.Image_Editor.brush_size / 2]
+    point_start_ll = [var.Image_Editor.brush_line_start[0] - var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] + var.Image_Editor.brush_size / 2]
+    point_start_lr = [var.Image_Editor.brush_line_start[0] + var.Image_Editor.brush_size / 2, var.Image_Editor.brush_line_start[1] + var.Image_Editor.brush_size / 2]
+
+    if var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_line_start[0] > 0 and var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_line_start[1] > 0:
+        pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], const.Color.erase, [point_start_ll, point_start_ul, point_start_ur, point_cur_ur, point_cur_lr, point_cur_ll])
+
+    if var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_line_start[0] > 0 and var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_line_start[1] <= 0:
+        pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], const.Color.eraser, [point_start_ul, point_start_ll, point_start_lr, point_cur_lr, point_cur_ur, point_cur_ul])
+
+    if var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_line_start[0] <= 0 and var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_line_start[1] > 0:
+        pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], const.Color.erase, [point_start_ul, point_start_ur, point_start_lr, point_cur_lr, point_cur_ll, point_cur_ul])
+
+    if var.Image_Editor.brush_line_current[0] - var.Image_Editor.brush_line_start[0] <= 0 and var.Image_Editor.brush_line_current[1] - var.Image_Editor.brush_line_start[1] <= 0:
+        pygame.draw.polygon(var.Image_Editor.layer[var.Image_Editor.layer_selected], const.Color.erase, [point_start_ur, point_start_lr, point_start_ll, point_cur_ll, point_cur_ul, point_cur_ur])
